b5e3cd64a4616b9344639f27a5d7a8ce80fc6825
Dairon Hernandez <daironh@gmail.com>
2018-08-15 00:14:49 -0400

    First commit

---
+++ src/main/java/com/daironh/authldap/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.daironh.authldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class HelloController {
+
+	@GetMapping("/check")
+	public String check() {
+		return "Check working";
+	}
+
+}
